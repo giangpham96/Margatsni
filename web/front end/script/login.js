929a2d8d8d2a83c4f42481f0f6aa5cb6dde8865f
cdrbbt <egor.bykadorov@gmail.com>
2017-11-30 12:46:39 +0200

    login page update
    script for tab switching
    small visual changes

--- web/front end/script/login.js
+++ web/front end/script/login.js
@@ -1,19 +1,19 @@
 'use strict';
-const tabs = document.querySelectorAll('li');
+const tabs = document.querySelectorAll('a');
 const panels = document.querySelectorAll('.tab');
-for(i = 0; i < tabs.length; i++) {
+for(var i = 0; i < tabs.length; i++) {
   var tab = tabs[i];
   setTabHandler(tab, i);
 }
 function setTabHandler(tab, tabPos) {
   tab.onclick = function() {
     for(i = 0; i < tabs.length; i++) {
-      tabs[i].className = '';
+      tabs[i].className = 'inactive';
     }
     tab.className = 'active';
     for(i = 0; i < panels.length; i++) {
-      panels[i].className = '';
+      panels[i].className = 'hidden';
     }
-    panels[tabPos].className = 'active-panel';
+    panels[tabPos].className = 'active-panel tab';
   }
 }
