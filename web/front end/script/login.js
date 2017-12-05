da555e932224c198cf5046cacb54d93574364ef3
cdrbbt <egor.bykadorov@gmail.com>
2017-12-05 14:25:37 +0200

    Logging in script test
    script for logging ina dded

--- web/front end/script/login.js
+++ web/front end/script/login.js
@@ -17,3 +17,81 @@ function setTabHandler(tab, tabPos) {
     panels[tabPos].className = 'active-panel tab';
   }
 }
+
+
+fetch('api/authorized', {
+    credentials: 'include', 
+    headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'auth-token': getCookie('auth-token')
+            }})
+        .then((response) => {
+            return response.json()
+        })
+        .then((json) => {
+            if (json.message == "authorized") {
+//                window.location.href = "https://10.114.32.118:8181/GET/feed.html"
+                return
+            }
+            initViews();
+        })
+        .catch((err) => {
+            console.log(err)
+        });
+
+initViews = () => {
+    setupLogin()
+    setupSignup()
+}
+
+setupLogin = () => {
+    const loginForm = document.getElementById('#logInbtn')
+    loginForm.onclick = function(){
+        const emailInput = document.querySelector('#logInForm > input[type="email"]');
+        const pwInput = document.querySelector('#logInFrom > input[type="password"]');
+        
+        fetch('api/authorized', {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            credentials: 'include',
+            method: 'POST',
+            body: `email=${emailInput.value}&password=${pwInput.value}`
+        })
+                .then((response) => {
+                    return response.json()
+                })
+                .then((json) => {
+                    if (json.error) {
+//                        errorMsg.innerHTML = json.error
+                        
+                    console.log(json.error)
+                        return
+                    }
+        document.cookie = "auth-token" + "=" + json['auth-token'] + ";" + "path=/";
+            
+//                    window.location.href = "https://10.114.32.118:8181/GET/feed.html"
+                })
+                .catch((err) => {
+                    console.log(err)
+                });
+
+    }
+}
+
+
+function getCookie(cname) {
+    const name = cname + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
--- web/javascript/login.js
+++ web/javascript/login.js
@@ -1,3 +1,4 @@
+'use strict';
 fetch('api/authorized', {
     credentials: 'include', 
     headers: {
