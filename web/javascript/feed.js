498d40239ecb30aa4201e9c0c7cbba343bbc9895
tripheo0412 <tripheo0412@gmail.com>
2017-12-07 13:28:48 +0200

    modify profile page and feed page

--- web/javascript/feed.js
+++ web/javascript/feed.js
@@ -5,18 +5,20 @@ var colWidth = 0;
 var margin = 20;
 var windowWidth = 0;
 var blocks = [];
-/*function setupBlocks() {
+function setupBlocks() {
     windowWidth = window.innerWidth;
-    colWidth = window.document.querySelectorAll(".block").clientWidth;
-    colCount = Math.floor(windowWidth/(colWidth+margin));
+    colWidth = Array.from(window.document.getElementsByClassName("block"), e => e.offsetWidth);
+    console.log(colWidth)
+    colCount = Math.floor(windowWidth / (colWidth[0]+margin));
     for(var i=0; i < colCount; i++) {
         blocks.push(margin);
     }
+    console.log(windowWidth)
     alert(blocks);
 }
 function positionBlocks() {
     var block = window.document.getElementsByTagName(".block");
-    for (var i=0, max = block.length)
+    for (var i=0, max = block.length;i<max;i++)
     $('.block').each(function(){
         var min = Array.min(blocks);
         var index = $.inArray(min, blocks);
@@ -33,37 +35,37 @@ function positionBlocks() {
 Array.min = function(array) {
     return Math.min.apply(Math, array);
 };
-*/
-$(function(){
-	$(window).resize(setupBlocks);
-});
 
-function setupBlocks() {
-	windowWidth = $(window).width();
-	colWidth = $('.block').outerWidth();
-	blocks = [];
-	console.log(blocks);
-	colCount = Math.floor(windowWidth/(colWidth+margin*2));
-	for(var i=0;i<colCount;i++){
-		blocks.push(margin);
-	}
-	positionBlocks();
-}
-
-function positionBlocks() {
-	$('.block').each(function(){
-		var min = Array.min(blocks);
-		var index = $.inArray(min, blocks);
-		var leftPos = margin+(index*(colWidth+margin));
-		$(this).css({
-			'left':leftPos+'px',
-			'top':min+'px'
-		});
-		blocks[index] = min+$(this).outerHeight()+margin;
-	});	
-}
-
-// Function to get the Min value in Array
-Array.min = function(array) {
-    return Math.min.apply(Math, array);
-};
+//$(function(){
+//	$(window).resize(setupBlocks);
+//});
+//
+//function setupBlocks() {
+//	windowWidth = $(window).width();
+//	colWidth = $('.block').outerWidth();
+//	blocks = [];
+//	console.log(blocks);
+//	colCount = Math.floor(windowWidth/(colWidth+margin*2));
+//	for(var i=0;i<colCount;i++){
+//		blocks.push(margin);
+//	}
+//	positionBlocks();
+//}
+//
+//function positionBlocks() {
+//	$('.block').each(function(){
+//		var min = Array.min(blocks);
+//		var index = $.inArray(min, blocks);
+//		var leftPos = margin+(index*(colWidth+margin));
+//		$(this).css({
+//			'left':leftPos+'px',
+//			'top':min+'px'
+//		});
+//		blocks[index] = min+$(this).outerHeight()+margin;
+//	});	
+//}
+//
+//// Function to get the Min value in Array
+//Array.min = function(array) {
+//    return Math.min.apply(Math, array);
+//};
