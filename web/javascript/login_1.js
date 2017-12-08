c1361c0daadd717e99453cb7ea3803bd1b59d9a9
cdrbbt <egor.bykadorov@gmail.com>
2017-12-08 14:17:45 +0200

    simple form validation
    error message when submit pressed
    disappears when input is valid again

--- web/javascript/login_1.js
+++ web/javascript/login_1.js
@@ -5,24 +5,167 @@ let validsu = false;
 let validli = false;
 
 //Set up tab switching
-for (let i = 0; i < tabs.length; i++) {
-    let tab = tabs[i];
-    setTabHandler(tab, i);
-}
-
 const setTabHandler = (tab, tabPos) => {
     tab.addEventListener('click', (evt) => {
-        for (i = 0; i < tabs.length; i++) {
+        for (let i = 0; i < tabs.length; i++) {
             tabs[i].className = 'inactive';
         }
         tab.className = 'active';
-        for (i = 0; i < panels.length; i++) {
+        for (let i = 0; i < panels.length; i++) {
             panels[i].className = 'hidden';
         }
         panels[tabPos].className = 'active-panel tab';
     });
 }
 
+for (let i = 0; i < tabs.length; i++) {
+    let tab = tabs[i];
+    setTabHandler(tab, i);
+}
+
+//Log in form set up
+const setupLogin = () => {
+    const loginForm = document.querySelector('#logInbtn')
+    loginForm.addEventListener('click', (evt) => {
+        const emailInput = document.querySelector('#logInForm > input[type="email"]');
+        const pwInput = document.querySelector('#logInForm > input[type="password"]');
+        const errorp = document.querySelector('#errorli');
+
+        emailInput.addEventListener('input', (evt) => {
+            if (emailInput.validity.valid) {
+                errorp.className = "invisible";
+            }
+        });
+
+        if (!emailInput.validity.valid) {
+            errorp.innerHTML = "invalid email";
+            errorp.className = "visible";
+        } else {
+            fetch('https://10.114.32.118:8181/GET/api/authorized', {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    },
+                    credentials: 'include',
+                    method: 'POST',
+                    body: `email=${emailInput.value}&password=${pwInput.value}`
+                })
+                .then((response) => {
+                    return response.json();
+                })
+                .then((json) => {
+                    if (json.message) {
+                        errorp.innerHTML = json.message;
+                        errorp.className = "visible";
+                        console.log(json.message);
+                        return;
+                    }
+                    document.cookie = "auth-token" + "=" + json['auth-token'] + ";" + "path=/";
+
+                    //                    window.location.href = "https://10.114.32.118:8181/GET/feed.html"
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+
+        }
+
+    });
+};
+
+
+//Sign up form set up
+const setupSignup = () => {
+    const loginForm = document.querySelector('#signUpbtn')
+    loginForm.addEventListener('click', (evt) => {
+        const unameInput = document.querySelector('#signUpForm > input[type="text"]');
+        const emailInput = document.querySelector('#signUpForm > input[type="email"]');
+        const pwInput = document.querySelector('#signUpForm > input[type="password"]');
+        const errorp = document.querySelector('#errorsu');
+
+        emailInput.addEventListener('input', (evt) => {
+            if (emailInput.validity.valid) {
+                errorp.className = "invisible";
+            }
+        });
+
+        pwInput.addEventListener('input', (evt) => {
+            if (pwInput.validity.valid) {
+                errorp.className = "invisible";
+            }
+        });
+
+        unameInput.addEventListener('input', (evt) => {
+            if (unameInput.validity.valid) {
+                errorp.className = "invisible";
+            }
+        });
+
+
+        if (!unameInput.validity.valid) {
+            errorp.innerHTML = "invalid username";
+            errorp.className = "visible";
+        } else if (!emailInput.validity.valid) {
+            errorp.innerHTML = "invalid email";
+            errorp.className = "visible";
+        } else if (!pwInput.validity.valid) {
+            errorp.innerHTML = "invalid password";
+            errorp.className = "visible";
+        } else {
+
+            fetch('https://10.114.32.118:8181/GET/api/signup', {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    },
+                    credentials: 'include',
+                    method: 'POST',
+                    body: `uname=${unameInput.value}&email=${emailInput.value}&password=${pwInput.value}`
+                })
+                .then((response) => {
+                    return response.json();
+                })
+                .then((json) => {
+                    if (json.message) {
+                        errorp.innerHTML = json.message;
+                        errorp.className = "visible";
+                        console.log(json.message);
+                        return;
+                    }
+                    document.cookie = "auth-token" + "=" + json['auth-token'] + ";" + "path=/";
+
+                    //window.location.href = "https://10.114.32.118:8181/GET/feed.html"
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        }
+
+    });
+};
+
+//Set up log in and sign up forms
+const initViews = () => {
+    setupLogin();
+    setupSignup();
+}
+
+initViews();
+
+//Checks cookie
+const getCookie = (cname) => {
+    const name = cname + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+};
 
 //Attempt to check if logged in
 fetch('https://10.114.32.118:8181/GET/api/authorized', {
@@ -46,104 +189,3 @@ fetch('https://10.114.32.118:8181/GET/api/authorized', {
     .catch((err) => {
         console.log(err);
     });
-
-
-//Set up log in and sign up forms
-const initViews = () => {
-    setupLogin();
-    setupSignup();
-}
-
-//Log in form set up
-const setupLogin = () => {
-    const loginForm = document.querySelector('#logInbtn')
-    loginForm.addEventListener('click', (evt) => {
-        const emailInput = document.querySelector('#logInForm > input[type="email"]');
-        const pwInput = document.querySelector('#logInForm > input[type="password"]');
-        const errorp = document.querySelector('#errorli');
-
-        fetch('https://10.114.32.118:8181/GET/api/authorized', {
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                credentials: 'include',
-                method: 'POST',
-                body: `email=${emailInput.value}&password=${pwInput.value}`
-            })
-            .then((response) => {
-                return response.json();
-            })
-            .then((json) => {
-                if (json.message) {
-                    errorp.innerHTML = json.message;
-                    errorp.className = "visible";
-                    console.log(json.message);
-                    return;
-                }
-                document.cookie = "auth-token" + "=" + json['auth-token'] + ";" + "path=/";
-
-                //                    window.location.href = "https://10.114.32.118:8181/GET/feed.html"
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-
-    });
-};
-
-
-//Sign up form set up
-const setupSignup = () => {
-    const loginForm = document.querySelector('#signUpbtn')
-    loginForm.addEventListener('click', (evt) => {
-        const unameInput = document.querySelector('#signUpForm > input[type="text"]');
-        const emailInput = document.querySelector('#signUpForm > input[type="email"]');
-        const pwInput = document.querySelector('#signUpForm > input[type="password"]');
-        const errorp = document.querySelector('#errorsu');
-
-        fetch('https://10.114.32.118:8181/GET/api/signup', {
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                credentials: 'include',
-                method: 'POST',
-                body: `uname=${unameInput.value}&email=${emailInput.value}&password=${pwInput.value}`
-            })
-            .then((response) => {
-                return response.json();
-            })
-            .then((json) => {
-                if (json.message) {
-                    errorp.innerHTML = json.message;
-                    errorp.className = "visible";
-                    console.log(json.message);
-                    return;
-                }
-                document.cookie = "auth-token" + "=" + json['auth-token'] + ";" + "path=/";
-
-                //window.location.href = "https://10.114.32.118:8181/GET/feed.html"
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-
-    });
-};
-
-
-//Checks cookie
-const getCookie = (cname) => {
-    const name = cname + "=";
-    const decodedCookie = decodeURIComponent(document.cookie);
-    const ca = decodedCookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) === 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-};
