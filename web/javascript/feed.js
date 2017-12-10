f1e044c03447ec63262042948734a22399d1aec3
giangp <conmeobeo96@gmail.com>
2017-12-10 17:11:18 +0200

    upload

--- web/javascript/feed.js
+++ web/javascript/feed.js
@@ -77,7 +77,7 @@ const like = (alikebutton, alikeno, post_id) => {
         },
         credentials: 'include',
         method: 'POST',
-        body: `post=${post_id}`
+        body: `post=${encodeURIComponent(post_id)}`
     })
             .then((response) => {
                 return response.json();
@@ -126,30 +126,31 @@ const createComment = (ulComment, textArea, post_id) => {
 
 const timeSince = (date) => {
 
-    const seconds = Math.floor((new Date() - date) / 1000);
+  let seconds = Math.floor((new Date() - date) / 1000);
 
-    let interval = Math.floor(seconds / 31536000);
+  let interval = Math.floor(seconds / 31536000);
 
-    if (interval > 1) {
-        return interval + " years ago";
-    }
-    interval = Math.floor(seconds / 2592000);
-    if (interval > 1) {
-        return interval + " months ago";
-    }
-    interval = Math.floor(seconds / 86400);
-    if (interval > 1) {
-        return interval + " days ago";
-    }
-    interval = Math.floor(seconds / 3600);
-    if (interval > 1) {
-        return interval + " hours ago";
-    }
-    interval = Math.floor(seconds / 60);
-    if (interval > 1) {
-        return interval + " minutes ago";
-    }
-    return Math.floor(seconds) + " seconds";
+  if (interval > 1) {
+    return interval + " years ago";
+  }
+  interval = Math.floor(seconds / 2592000);
+  if (interval > 1) {
+    return interval + " months ago";
+  }
+  interval = Math.floor(seconds / 86400);
+  if (interval > 1) {
+    return interval + " days ago";
+  }
+  interval = Math.floor(seconds / 3600);
+  if (interval > 1) {
+    return interval + " hours ago";
+  }
+  interval = Math.floor(seconds / 60);
+  if (interval > 1) {
+    return interval + " minutes ago";
+  }
+  if (seconds < 0) seconds = 0;
+  return Math.floor(seconds) + " seconds";
 };
 
 const renderComment = (c) => {
